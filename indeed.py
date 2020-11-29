a59fae86a17a4837a3849102bf59f75fa1e42f97
Rayin42 <dgjosh@naver.com>
2020-11-30 01:01:49 +0900

    Created extract_indeed_jobs
    Extract title, company name

--- indeed.py
+++ indeed.py
@@ -22,4 +22,15 @@ def extract_indeed_jobs(last_page):
     for page in range(last_page):
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            title = result.find("h2", {"class": "title"}).find("a")["title"]
+            company = result.find("span", {"class": "company"})
+            company_anchor = company.find("a")
+            if company_anchor is not None:
+                company = (str(company_anchor.string))
+            else:
+                company = (str(company.string))
+            company = company.strip()
+            print(title, company)
     return jobs
